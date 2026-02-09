35ce8d1ad6c35d7c46be49985931f93b6e9a5234
CircusHan <smcrony010@gmail.com>
2026-02-09 17:27:49 +0900

    876 fast slow Done.

---
+++ 876_fast_slowpointers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
