29b56312c5d6244f16da30a5e132914198cb7a45
CircusHan <smcrony010@gmail.com>
2026-02-10 13:39:22 +0900

    Binary Tree level Order Done.

--- 94.py
+++ 94.py
@@ -70,11 +70,66 @@ class Solution:
         postorder(root)
         return result
 
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        #final_list 라는 최종 아웃풋 필요
+        #deque가 필요함
+        #c_list가 필요함
+
+        #1. 빈 트리가 있으면 빈 리스트 반환
+        if not root:
+            return []
+        #2.초기화 final_list, queue, current_list, last_level
+        final_list = []
+        q = deque()
+        c_list = []
+        last_level = 0
+
+        q.append(root)
+        while q:
+            c_list = []
+            for _ in range(len(q)): # for all nodes at the same level of a queue
+                n = q.popleft()
+                c_list.append(n.val)
+                if n.left:
+                    q.append(n.left)
+                if n.right:
+                    q.append(n.right)
+            
+            final_list.append(c_list)
+        return final_list
+        
+        '''
+        #3. queue의 (root,0)을 push
+        q.append((root, 0))
+        #4. while queue is not empty
+        while q:
+            #5. (node, level) = queue.pop()
+            (n, level) = q.popleft()
+            #6. 레벨이 바뀌었으면 이전의 current list를 final_list에 append하기. current_list 초기화하기
+            if level > last_level:
+                last_level += 1
+                final_list.append(c_list)
+                c_list = []
+            #7. current_list에다 node.val을 append하기.
+            c_list.append(n.val)
+            #8. current의 자식 노드에 대해서 자식을 queue에 넣기 + 레벨 증가.
+            if n.left:
+                q.append((n.left, level+1))
+            if n.right:
+                q.append((n.right, level+1))
+            
+        #final_list에 남아있는 current_list 넣기
+        final_list.append(c_list)
+        # print(final_list)
+        return final_list
+        '''
+        
+        
 
 arr = [1,2,3,4,5]
 root = array_to_tree(arr)
 sol = Solution()
-print(sol.inorderTraversal(root))
-print(sol.preorderTraversal(root))
-print(sol.postorderTraversal(root))
-
+# print(sol.inorderTraversal(root))
+# print(sol.preorderTraversal(root))
+# print(sol.postorderTraversal(root))
+print(sol.levelOrder(root))
